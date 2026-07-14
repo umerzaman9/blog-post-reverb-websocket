8f838f30d613524b61c5b25a3d14cbc6ca6bcb69
Umer Zaman <umer.zaman@techswivel.com>
2026-07-14 15:42:57 +0500

    seeded posts

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory()->create([
+            'name'     => 'Umar',
+            'email'    => 'umar@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        Post::factory(10)->create([
+            'userId' => $user->id,
+        ]);
+    }
+}
