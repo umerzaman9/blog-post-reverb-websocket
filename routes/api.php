dd16a203a06d77cad639e3eb7204d411a06dcc0e
Umer Zaman <umer.zaman@techswivel.com>
2026-07-14 15:46:50 +0500

    defining routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CommentController;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/posts/{post}', function (Post $post) {
+    return view('posts.show', compact('post'));
+})->name('posts.show');
+
+Route::get('/posts/{post}/comments', [CommentController::class, 'index']); // guests OK
+Route::middleware('auth')->group(function () {
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
 });
